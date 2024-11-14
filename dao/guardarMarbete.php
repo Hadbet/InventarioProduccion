b374a8bdc82f0b713744794e064ca349cab7d869
hadbet <hadbet.altamirano@grammer.com>
2024-11-14 10:00:35 -0600

    PRE PRUEBAS

--- dao/guardarMarbete.php
+++ dao/guardarMarbete.php
@@ -20,20 +20,32 @@ $Object = new DateTime();
 $Object->setTimezone(new DateTimeZone('America/Denver'));
 $DateAndTime = $Object->format("Y/m/d h:i:s");
 
-$stmt = $conex->prepare("INSERT INTO `Bitacora_Inventario`(`StorageUnit`, `NumeroParte`, `Cantidad`, `FolioMarbete`, `Fecha`, `Usuario`, `Estatus`, `Conteo`, `Comentario`) VALUES (?, ?, ?, ?, ?, ?, '1', ?, ?)");
+$stmt = $conex->prepare("INSERT INTO `Bitacora_Inventario`(`NumeroParte`, `FolioMarbete`, `Fecha`, `Usuario`, `Estatus`, `PrimerConteo`, `SegundoConteo`, `TercerConteo`, `Comentario`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
+$totalCantidad = 0;
 foreach ($storageUnits as $storageUnit => $details) {
     $numeroParte = $details['numeroParte'];
     $cantidad = $details['cantidad'];
-    $stmt->bind_param("ssssssis", $storageUnit, $numeroParte, $cantidad, $marbete, $DateAndTime, $nombre,$conteo,$comentarios);
-    if (!$stmt->execute()) {
-        array_push($failedUnits, $storageUnit);
-    }
+    $totalCantidad += $cantidad;
 }
 
-if (count($failedUnits) > 0) {
-    echo json_encode(["success" => false, "failedUnits" => $failedUnits]);
+$primerConteo = $conteo == 1 ? $totalCantidad : 0;
+$segundoConteo = $conteo == 2 ? $totalCantidad : 0;
+$tercerConteo = $conteo == 3 ? $totalCantidad : 0;
+
+$stmt->bind_param("ssssiiiss", $numeroParte, $marbete, $DateAndTime, $nombre, $conteo, $primerConteo, $segundoConteo, $tercerConteo, $comentarios);
+
+if (!$stmt->execute()) {
+    echo json_encode(["success" => false]);
 } else {
+    // Actualizar el estatus de los Storage_Unit
+    $stmt2 = $conex->prepare("UPDATE `Storage_Unit` SET `Estatus`='1' WHERE `Id_StorageUnit` = ?");
+    foreach ($storageUnits as $storageUnit => $details) {
+        $idStorageUnit = $details['idStorageUnit'];
+        $stmt2->bind_param("s", $idStorageUnit);
+        $stmt2->execute();
+    }
+    $stmt2->close();
     echo json_encode(["success" => true]);
 }
 
--- form_registro.php
+++ form_registro.php
@@ -261,10 +261,10 @@
 
         var marbete = document.getElementById("scanner_input").value.split('.')[0];
 
-        $.getJSON('https://grammermx.com/Inventario/dao/consultaMarbete.php?marbete='+marbete, function (data) {
+        $.getJSON('https://grammermx.com/Logistica/Inventario/dao/consultaMarbete.php?marbete='+marbete, function (data) {
             for (var i = 0; i < data.data.length; i++) {
                 if (data.data[i].FolioMarbete) {
-                    if (data.data[i].Estatus === '1'){
+                    if (data.data[i].Estatus === '0'){
                         numeroParte=data.data[i].Numero_Parte;
                         storageBin=data.data[i].StorageBin;
                         document.getElementById("reader").style.display = 'none';
@@ -295,22 +295,34 @@
 
         var marbete = decodedText.split('.')[0];
 
-        $.getJSON('https://grammermx.com/Inventario/dao/consultaMarbete.php?marbete='+marbete, function (data) {
-            for (var i = 0; i < data.data.length; i++) {
-                if (data.data[i].Id_Marbete) {
-                    numeroParte=data.data[i].Numero_Parte;
-                    storageBin=data.data[i].StorageBin;
-                    console.log(`Code matched = ${decodedText}`, decodedResult);
-                    document.getElementById("scanner_input").value = decodedText;
-                    document.getElementById("reader").style.display = 'none';
-                    //document.getElementById("lblNumeroParte").innerText = "Número de parte : "+numeroParte;
-                    document.getElementById("Ubicacion").innerHTML = "Ubicación : "+storageBin;
-                    document.getElementById("pasoDos").style.display = 'block';
-                    document.getElementById("pasoUno").style.display = 'none';
-                    html5QrcodeScanner.clear();
-                    html5QrcodeScanner.pause();
+        $.getJSON('https://grammermx.com/Logistica/Inventario/dao/consultaMarbete.php?marbete='+marbete, function (data) {
+            for (var i = 0; i < data.data.length; i++) {{
+                if (data.data[i].FolioMarbete) {
+                    if (data.data[i].Estatus === '0'){
+                        numeroParte=data.data[i].Numero_Parte;
+                        storageBin=data.data[i].StorageBin;
+                        console.log(`Code matched = ${decodedText}`, decodedResult);
+                        document.getElementById("scanner_input").value = decodedText;
+                        document.getElementById("reader").style.display = 'none';
+                        //document.getElementById("lblNumeroParte").innerText = "Número de parte : "+numeroParte;
+                        document.getElementById("Ubicacion").innerHTML = "Ubicación : "+storageBin;
+                        document.getElementById("pasoDos").style.display = 'block';
+                        document.getElementById("pasoUno").style.display = 'none';
+                        html5QrcodeScanner.clear();
+                        html5QrcodeScanner.pause();
+                    }else{
+                        Swal.fire({
+                            title: "El marbete ya fue registrado",
+                            text: "Escanea otro marbete",
+                            icon: "error"
+                        });
+                    }
                 } else {
-                    alert('Id_Marbete no existe en el objeto data');
+                    Swal.fire({
+                        title: "El marbete no esta cargado",
+                        text: "Verificalo con la mesa central",
+                        icon: "error"
+                    });
                 }
             }
         });
