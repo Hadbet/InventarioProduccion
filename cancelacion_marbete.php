7eb2ca8be6ed88454ffadfda3e913effb245e451
hadbet <hadbet.altamirano@grammer.com>
2024-11-20 16:52:47 -0600

    PRE PRUEBAS

--- cancelacion_marbete.php
+++ cancelacion_marbete.php
@@ -191,7 +191,7 @@ if (strlen($nomina) == 7) {
 
       function actualizarTabla() {
           $.ajax({
-              url: 'https://grammermx.com/Logistica/Inventario/dao/consultaMarbeteLider.php?area='+<?php echo $area; ?>, // Reemplaza esto con la URL de tus datos
+              url: 'https://grammermx.com/Logistica/Inventario/dao/consultaMarbeteLider.php?area=<?php echo $area; ?>', // Reemplaza esto con la URL de tus datos
               dataType: 'json',
               success: function(data) {
                   var table = $('#dataTable-1').DataTable();
