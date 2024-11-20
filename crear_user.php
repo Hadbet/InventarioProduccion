80da8391ab02008c5d6e87645f58a1602bb14bd0
hadbet <hadbet.altamirano@grammer.com>
2024-11-20 12:15:05 -0600

    PRE PRUEBAS

--- crear_user.php
+++ crear_user.php
@@ -73,7 +73,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-md-1" >
+                            <div class="col-md-2" >
                                 <div class="form-group mb-3">
                                     <label for="cbInstructor">Rol</label>
                                     <select class="custom-select" id="cbRol">
