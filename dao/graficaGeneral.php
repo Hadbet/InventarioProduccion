88c484188b4bbeda7c906dd955e0f446a1ad86a7
hadbet <hadbet.altamirano@grammer.com>
2024-11-20 08:38:56 -0600

    PRE PRUEBAS

--- dao/graficaGeneral.php
+++ dao/graficaGeneral.php
@@ -20,12 +20,12 @@ function ContadorApu()
     COUNT(CASE WHEN BI.`SegundoConteo` = 0 AND BI.`Estatus` IN (0, 1) THEN 1 END) AS 'SegundoConteoNoLiberado',
     COUNT(CASE WHEN BI.`TercerConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) AS 'TercerConteoLiberado',
     COUNT(CASE WHEN BI.`TercerConteo` = 0 AND BI.`Estatus` IN (0, 1) THEN 1 END) AS 'TercerConteoNoLiberado',
-    COUNT(BI.`PrimerConteo`) AS 'TotalPrimerConteo',
-    COUNT(BI.`SegundoConteo`) AS 'TotalSegundoConteo',
-    COUNT(BI.`TercerConteo`) AS 'TotalTercerConteo',
-    (COUNT(CASE WHEN BI.`PrimerConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) / COUNT(BI.`PrimerConteo`)) * 100 AS 'PorcentajePrimerConteo',
-    (COUNT(CASE WHEN BI.`SegundoConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) / COUNT(BI.`SegundoConteo`)) * 100 AS 'PorcentajeSegundoConteo',
-    (COUNT(CASE WHEN BI.`TercerConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) / COUNT(BI.`TercerConteo`)) * 100 AS 'PorcentajeTercerConteo'
+    COUNT(CASE WHEN BI.`Estatus` = 1 THEN BI.`PrimerConteo` END) AS 'TotalPrimerConteo',
+    COUNT(CASE WHEN BI.`Estatus` = 1 THEN BI.`SegundoConteo` END) AS 'TotalSegundoConteo',
+    COUNT(CASE WHEN BI.`Estatus` = 1 THEN BI.`TercerConteo` END) AS 'TotalTercerConteo',
+    (COUNT(CASE WHEN BI.`PrimerConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) / COUNT(CASE WHEN BI.`Estatus` = 1 THEN BI.`PrimerConteo` END)) * 100 AS 'PorcentajePrimerConteo',
+    (COUNT(CASE WHEN BI.`SegundoConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) / COUNT(CASE WHEN BI.`Estatus` = 1 THEN BI.`SegundoConteo` END)) * 100 AS 'PorcentajeSegundoConteo',
+    (COUNT(CASE WHEN BI.`TercerConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) / COUNT(CASE WHEN BI.`Estatus` = 1 THEN BI.`TercerConteo` END)) * 100 AS 'PorcentajeTercerConteo'
 FROM 
     `Area` AS A
 LEFT JOIN 
