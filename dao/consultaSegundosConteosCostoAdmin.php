50defbb58a56831542757ae4975f1fc3c37a3172
hadbet <hadbet.altamirano@grammer.com>
2024-11-27 16:16:24 -0600

    PRE PRUEBAS

--- dao/consultaSegundosConteosCostoAdmin.php
+++ dao/consultaSegundosConteosCostoAdmin.php
@@ -13,11 +13,23 @@ function ContadorApu()
     $datos = mysqli_query($conex, "SELECT * FROM (
     SELECT 
         (SELECT SUM(Cantidad) FROM InventarioSap) AS TotalInventarioSap, 
-        (SELECT SUM(PrimerConteo) FROM Bitacora_Inventario WHERE  Estatus = 1) AS TotalPrimerConteoBitacora, 
+        (SELECT SUM(
+            COALESCE( 
+                CASE WHEN TercerConteo != 0 THEN TercerConteo END, 
+                CASE WHEN SegundoConteo != 0 THEN SegundoConteo END, 
+                PrimerConteo 
+            )
+        ) FROM Bitacora_Inventario WHERE  Estatus = 1) AS TotalPrimerConteoBitacora, 
         (SELECT SUM(InventarioSap.Cantidad * (Parte.Costo / Parte.Por)) 
          FROM InventarioSap 
          INNER JOIN Parte ON InventarioSap.GrammerNo = Parte.GrammerNo) AS CostoTotalInventarioSap, 
-        (SELECT SUM(Bitacora_Inventario.PrimerConteo * (Parte.Costo / Parte.Por)) 
+        (SELECT SUM(
+            COALESCE( 
+                CASE WHEN Bitacora_Inventario.TercerConteo != 0 THEN Bitacora_Inventario.TercerConteo END, 
+                CASE WHEN Bitacora_Inventario.SegundoConteo != 0 THEN Bitacora_Inventario.SegundoConteo END, 
+                Bitacora_Inventario.PrimerConteo 
+            ) * (Parte.Costo / Parte.Por)
+        ) 
          FROM Bitacora_Inventario 
          INNER JOIN Parte ON Bitacora_Inventario.NumeroParte = Parte.GrammerNo 
          WHERE Bitacora_Inventario.Estatus = 1) AS CostoTotalPrimerConteoBitacora
