6d1dab4a6809e5480c2df232029a775825fc687d
hadbet <hadbet.altamirano@grammer.com>
2024-11-28 16:09:18 -0600

    PRE PRUEBAS

--- form_registro_coordinador.php
+++ form_registro_coordinador.php
@@ -270,7 +270,7 @@ if (strlen($nomina) == 7) {
                                 var cell1 = row.insertCell(0);
                                 var cell2 = row.insertCell(1);
                                 var cell3 = row.insertCell(2);
-                                cell1.innerHTML = data.data[i].Id_StorageUnit;
+                                cell1.innerHTML = data.data[i].StorageUnit;
                                 cell2.innerHTML = numeroParte;
                                 cell3.innerHTML = cantidad;
 
