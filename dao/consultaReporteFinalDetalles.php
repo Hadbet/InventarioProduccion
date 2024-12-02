6ffc95b337ab36d0fb59473cb719073b417b5171
hadbet <hadbet.altamirano@grammer.com>
2024-12-02 15:32:46 -0600

    PRE PRUEBAS

--- dao/consultaReporteFinalDetalles.php
+++ dao/consultaReporteFinalDetalles.php
@@ -16,11 +16,7 @@ function ContadorApu()
                 CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
                 CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            ) THEN ISap.Cantidad - COALESCE( 
-                CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
-                CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
-                BInv.PrimerConteo 
-            )
+            , 0) OR BInv.NumeroParte IS NULL THEN 1
             ELSE 0
         END
     ) AS 'Cantidad_Total_Negativa',
@@ -30,11 +26,7 @@ function ContadorApu()
                 CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
                 CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            ) THEN COALESCE( 
-                CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
-                CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
-                BInv.PrimerConteo 
-            ) - ISap.Cantidad
+            , 0) AND BInv.Estatus = 1 THEN 1
             ELSE 0
         END
     ) AS 'Cantidad_Total_Positiva',
@@ -44,11 +36,11 @@ function ContadorApu()
                 CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
                 CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            ) THEN (ISap.Cantidad - COALESCE( 
+            , 0) OR BInv.NumeroParte IS NULL THEN (ISap.Cantidad - COALESCE( 
                 CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
                 CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            )) * (Parte.Por / Parte.Costo)
+            , 0)) * (Parte.Por / Parte.Costo)
             ELSE 0
         END
     ) AS 'Costo_Total_Negativo',
@@ -58,20 +50,20 @@ function ContadorApu()
                 CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
                 CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            ) THEN (COALESCE( 
+            , 0) AND BInv.Estatus = 1 THEN (COALESCE( 
                 CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
                 CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
                 BInv.PrimerConteo 
-            ) - ISap.Cantidad) * (Parte.Por / Parte.Costo)
+            , 0) - ISap.Cantidad) * (Parte.Por / Parte.Costo)
             ELSE 0
         END
     ) AS 'Costo_Total_Positivo'
 FROM 
     InventarioSap ISap
-JOIN 
+LEFT JOIN 
     Bitacora_Inventario BInv ON ISap.GrammerNo = BInv.NumeroParte AND ISap.STBin = BInv.StorageBin
 JOIN 
-    Parte ON ISap.GrammerNo = Parte.GrammerNo;");
+    Parte ON ISap.GrammerNo = Parte.GrammerNo");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
