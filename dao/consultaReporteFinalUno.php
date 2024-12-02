e41e500372a2d4732d3365ba0b367a130f39f4bd
hadbet <hadbet.altamirano@grammer.com>
2024-12-02 09:07:25 -0600

    PRE PRUEBAS

---
+++ dao/consultaReporteFinalUno.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once('db/db_Inventario.php');
+
+ContadorApu();
+
+function ContadorApu()
+{
+    $con = new LocalConector();
+    $conex = $con->conectar();
+
+    $datos = mysqli_query($conex, "SELECT 
+    P.GrammerNo, 
+    P.Descripcion, 
+    P.UM, 
+    P.Costo / P.Por AS 'Costo_Unitario', 
+    SUM(
+        COALESCE( 
+            CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
+            CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
+            BInv.PrimerConteo 
+        )
+    ) AS 'Total_Bitacora_Inventario', 
+    ISap.Cantidad AS 'Total_InventarioSap', 
+    ISap.Cantidad - SUM(
+        COALESCE( 
+            CASE WHEN BInv.TercerConteo != 0 THEN BInv.TercerConteo END, 
+            CASE WHEN BInv.SegundoConteo != 0 THEN BInv.SegundoConteo END, 
+            BInv.PrimerConteo 
+        )
+    ) AS 'Diferencia'
+FROM 
+    Parte P
+LEFT JOIN 
+    InventarioSap ISap ON P.GrammerNo = ISap.GrammerNo
+LEFT JOIN 
+    Bitacora_Inventario BInv ON P.GrammerNo = BInv.NumeroParte AND BInv.Estatus = 1
+GROUP BY 
+    P.GrammerNo;");
+
+    $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+    echo json_encode(array("data" => $resultado));
+}
+
+
+?>
