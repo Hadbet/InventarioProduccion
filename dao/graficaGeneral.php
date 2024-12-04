f295a976cd1db51e6d885d4d4f901797e2cd69c0
hadbet <hadbet.altamirano@grammer.com>
2024-12-04 15:18:56 -0600

    cambio de consulta de graficaGeneral.php

--- dao/graficaGeneral.php
+++ dao/graficaGeneral.php
@@ -14,18 +14,18 @@ function ContadorApu()
 
     $datos = mysqli_query($conex, "SELECT 
     A.`AreaNombre`,
-    COUNT(CASE WHEN BI.`PrimerConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) AS 'PrimerConteoLiberado',
-    COUNT(CASE WHEN BI.`PrimerConteo` = 0 AND BI.`Estatus` IN (0, 1) THEN 1 END) AS 'PrimerConteoNoLiberado',
-    COUNT(CASE WHEN BI.`SegundoConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) AS 'SegundoConteoLiberado',
-    COUNT(CASE WHEN BI.`SegundoConteo` = 0 AND BI.`Estatus` IN (0, 1) THEN 1 END) AS 'SegundoConteoNoLiberado',
-    COUNT(CASE WHEN BI.`TercerConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) AS 'TercerConteoLiberado',
-    COUNT(CASE WHEN BI.`TercerConteo` = 0 AND BI.`Estatus` IN (0, 1) THEN 1 END) AS 'TercerConteoNoLiberado',
+    COUNT(CASE WHEN BI.`Estatus` = 1 THEN 1 END) AS 'PrimerConteoLiberado',
+    COUNT(CASE WHEN BI.`Estatus` = 0 THEN 1 END) AS 'PrimerConteoNoLiberado',
+    COUNT(CASE WHEN BI.`SegFolio` = 1 THEN 1 END) AS 'SegundoConteoLiberado',
+    COUNT(CASE WHEN BI.`SegFolio` = 2 THEN 1 END) AS 'SegundoConteoNoLiberado',
+    COUNT(CASE WHEN BI.`Estatus` = 1 THEN 1 END) AS 'TercerConteoLiberado',
+    COUNT(CASE WHEN BI.`Estatus` = 0 THEN 1 END) AS 'TercerConteoNoLiberado',
     COUNT(CASE WHEN BI.`Estatus` IN (0, 1,2) THEN 1 END) AS 'TotalPrimerConteo',
-    COUNT(CASE WHEN BI.`SegundoConteo` > 0 THEN 1 END) AS 'TotalSegundoConteo',
-    COUNT(CASE WHEN BI.`TercerConteo` > 0 THEN 1 END) AS 'TotalTercerConteo',
-    (COUNT(CASE WHEN BI.`PrimerConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) / COUNT(CASE WHEN BI.`Estatus` IN (0, 1,2) THEN 1 END)) * 100 AS 'PorcentajePrimerConteo',
-    (COUNT(CASE WHEN BI.`SegundoConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) / COUNT(CASE WHEN BI.`SegundoConteo` > 0 THEN 1 END)) * 100 AS 'PorcentajeSegundoConteo',
-    (COUNT(CASE WHEN BI.`TercerConteo` > 0 AND BI.`Estatus` = 1 THEN 1 END) / COUNT(CASE WHEN BI.`TercerConteo` > 0 THEN 1 END)) * 100 AS 'PorcentajeTercerConteo'
+    COUNT(CASE WHEN BI.`SegFolio` IS NOT NULL THEN 1 END) AS 'TotalSegundoConteo',
+    COUNT(CASE WHEN BI.`TercerConteo` IS NOT NULL THEN 1 END) AS 'TotalTercerConteo',
+    (COUNT(CASE WHEN BI.`Estatus` = 1 THEN 1 END) / COUNT(CASE WHEN BI.`Estatus` IN (0, 1,2) THEN 1 END)) * 100 AS 'PorcentajePrimerConteo',
+    (COUNT(CASE WHEN BI.`SegFolio` = 1 THEN 1 END) / COUNT(CASE WHEN BI.`SegFolio` IS NOT NULL THEN 1 END)) * 100 AS 'PorcentajeSegundoConteo',
+    (COUNT(CASE WHEN BI.`Estatus` = 1 THEN 1 END) / COUNT(CASE WHEN BI.`TercerConteo` IS NOT NULL THEN 1 END)) * 100 AS 'PorcentajeTercerConteo'
 FROM 
     `Area` AS A
 LEFT JOIN 
