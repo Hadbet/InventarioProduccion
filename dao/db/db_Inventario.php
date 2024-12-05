23202df383ad42d24dec88765cae64e1a89bebb4
hadbet <hadbet.altamirano@grammer.com>
2024-12-05 15:07:07 -0600

    control conteo

--- dao/db/db_Inventario.php
+++ dao/db/db_Inventario.php
@@ -1,18 +1,12 @@
 <?php
 
 class LocalConector
-{/*
+{
     private $host = "127.0.0.1:3306";
     private $usuario = "u909553968_UserInventario";
     private $clave = "Grammer2024Inventario";
     private $db = "u909553968_Inventario";
     public $conexion;
-*/
-    private $host = "127.0.0.1:3306";
-    private $usuario = "u909553968_UserInventario";
-    private $clave = "Grammer2024Inventario";
-    private $db = "u909553968_Inventario";
-    private $conexion;
 
     public function conectar()
     {
