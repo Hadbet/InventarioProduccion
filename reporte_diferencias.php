c18ec8b9fc7a463e2a0ba56f52b8d347e247d22d
hadbet <hadbet.altamirano@grammer.com>
2024-12-08 23:30:55 -0600

    control conteo

--- reporte_diferencias.php
+++ reporte_diferencias.php
@@ -152,7 +152,7 @@ if (strlen($nomina) == 7) {
             if (data && data.data && data.data.length > 0) {
                 for (var i = 0; i < data.data.length; i++) {
                     document.getElementById("lblDinero").innerText = (parseFloat(data.data[i].CostoTotalInventarioSap - data.data[i].CostoTotalPrimerConteoBitacora).toLocaleString("es-MX", {style: "currency", currency: "MXN"}));
-                    document.getElementById("lblCantidad").innerText = data.data[i].TotalInventarioSap-data.data[i].TotalPrimerConteoBitacora;
+                    document.getElementById("lblCantidad").innerText = (data.data[i].TotalInventarioSap - data.data[i].TotalPrimerConteoBitacora).toFixed(2);
                     crearTabla();
                 }
             }else{
