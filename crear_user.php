bc88ae4f4ce07237ff2dfbabe44945633b870130
hadbet <hadbet.altamirano@grammer.com>
2024-12-11 17:22:36 -0600

    focus

--- crear_user.php
+++ crear_user.php
@@ -104,12 +104,13 @@
                     </div>
                 </div> <!-- / .card -->
 
-
-              <h2 class="mb-2 page-title">Lista de marbetes capturados</h2>
                 <button type="button" onclick="masivo(1);" class="btn mb-2 btn-success float-right text-white">Habilitar todos<span
                             class="fe fe-send fe-16 ml-2"></span></button>
                 <button type="button" onclick="masivo(0);" class="btn mb-2 btn-danger float-right text-white">Deshabilitar todos<span
                             class="fe fe-send fe-16 ml-2"></span></button>
+
+              <h2 class="mb-2 page-title">Lista de marbetes capturados</h2>
+
               <div class="row my-4">
                 <!-- Small table -->
                 <div class="col-md-12">
