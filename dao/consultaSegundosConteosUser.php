3c8ca1429543c4cdb0d9addc45c131b81515ba27
hadbet <hadbet.altamirano@grammer.com>
2024-12-11 13:26:04 -0600

    focus

--- dao/consultaSegundosConteosUser.php
+++ dao/consultaSegundosConteosUser.php
@@ -31,6 +31,7 @@ LEFT JOIN
 WHERE 
     B.Area = $area
     AND B.Estatus = 1
+    AND (B.SegFolio = 1 OR B.SegFolio = 2)
     AND (I.GrammerNo IS NULL 
     OR B.PrimerConteo != I.Cantidad
     OR I.Cantidad = 0
@@ -64,6 +65,7 @@ WHERE
     OR B.PrimerConteo != I.Cantidad
     OR I.Cantidad = 0
     OR IFNULL(B.PrimerConteo, 0) = 0
+    OR (B.SegFolio = 1 OR B.SegFolio = 2)
     OR ABS(IFNULL(B.PrimerConteo, 0) - I.Cantidad) >= 10000)
 HAVING
     ABS(CostoInventarioSap - CostoBitacora) > 3000
