4ce45f7144cac930e99f0dcff1916742bd06b4cd
hadbet <hadbet.altamirano@grammer.com>
2024-12-12 15:38:10 -0600

    focus

--- dao/masivoCancelacion.php
+++ dao/masivoCancelacion.php
@@ -7,6 +7,8 @@ $ids = json_decode($_POST['ids']);
 try {
     $con = new LocalConector();
     $conex=$con->conectar();
+    $success = true;
+    $message = "Update exitosa";
 
     foreach ($ids as $id) {
         $stmt = $conex->prepare("UPDATE `Bitacora_Inventario` SET `Estatus`='5',`Comentario`=? WHERE `FolioMarbete` = ?");
@@ -14,15 +16,17 @@ try {
 
         $stmt->execute();
 
-        if ($stmt->affected_rows > 0) {
-            echo json_encode(["success" => true, "message" => "Update exitosa"]);
-        } else {
-            echo json_encode(["success" => false, "message" => "No se pudo insertar el registro UPDATE `Bitacora_Inventario` SET `Estatus`='5',`Comentario`=$comentario WHERE `Id_Bitacora` = $id"]);
+        if ($stmt->affected_rows <= 0) {
+            $success = false;
+            $message = "No se pudo insertar el registro UPDATE `Bitacora_Inventario` SET `Estatus`='5',`Comentario`=$comentario WHERE `Id_Bitacora` = $id";
+            break;
         }
     }
 
     $stmt->close();
     $conex->close();
+
+    echo json_encode(["success" => $success, "message" => $message]);
 
 } catch (Exception $e) {
     http_response_code(500);
